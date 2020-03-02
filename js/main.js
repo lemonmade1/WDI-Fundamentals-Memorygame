bdff440d93bd28ed3ff8afc16fa7b5f280d92dfc
Terrell Lemons <reddfoxx@Terrells-MBP.home>
2020-03-01 17:23:51 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
 const cards = [
-  "queen", 
-  "queen", 
-  "king", 
-  "king"
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  },
 ];
 
 const cardsInPlay = [];
 
 function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
-    console.log("You found a match.");
+    alert("You found a match.");
   } else {
-    console.log("Sorry, try again.")
+    alert("Sorry, try again.")
   }
 }
 
 function flipCard(cardId) {
 
-  cardsInPlay.push(cardId);
-  console.log(`User flipped the ${cards[cardId]}`);
-  
+  cardsInPlay.push(cards[cardId].rank);
+  console.log(`User flipped the ${cards[cardId].rank}`);
+
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
+
   if (cardsInPlay.length === 2) {
     checkForMatch();
   }

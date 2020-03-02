32246433a77b49772a260783e931face81a8de87
Terrell Lemons <reddfoxx@Terrells-MBP.home>
2020-03-01 16:17:40 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const cardOne = "Queen";
+const cardTwo = "Queen";
+const cardThree = "King";
+const cardFour = "King";
+
+console.log(`User flipped the ${cardOne}`);
+console.log(`User flipped the ${cardThree}`);
